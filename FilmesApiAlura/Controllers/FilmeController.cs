ca2c93caeea9d4e1c66190dece9c703daa32b5fd
Wilson Santos <contato@wilsonsantos.net>
2022-01-11 10:45:21 -0300

    Add project files.

---
+++ FilmesApiAlura/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApiAlura.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesApiAlura.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            
+        }
+    }
+}
---
+++ FilmesApiAlura/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApiAlura.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage ="O Campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor eh obrigatorio")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(10,400, ErrorMessage ="A duracao deve ter no minimo 10 e no maximo 400 minutos")]
+        public int Duracao { get; set; }
+    }
+}
