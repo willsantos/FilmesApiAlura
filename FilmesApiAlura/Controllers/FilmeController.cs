4cecaa5d85e144c4212547de0a86ddc9aed418e0
Wilson Santos <contato@wilsonsantos.net>
2022-01-11 11:49:28 -0300

    :sparkles: feat: Add recover data
    Methods:
    
    POST
    GET
    GetByID

--- FilmesApiAlura/Controllers/FilmeController.cs
+++ FilmesApiAlura/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FilmesApiAlura.Controllers
 {
@@ -10,12 +11,35 @@ namespace FilmesApiAlura.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme)
+        public IActionResult AdicionaFilme([FromBody]Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id},filme);
             
         }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+           Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+           if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+
+        }
+
     }
 }
--- FilmesApiAlura/Models/Filme.cs
+++ FilmesApiAlura/Models/Filme.cs
@@ -4,11 +4,17 @@ namespace FilmesApiAlura.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+        
         [Required(ErrorMessage ="O Campo titulo é obrigatorio")]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "O campo diretor eh obrigatorio")]
         public string Diretor { get; set; }
+        
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         public string Genero { get; set; }
+
         [Range(10,400, ErrorMessage ="A duracao deve ter no minimo 10 e no maximo 400 minutos")]
         public int Duracao { get; set; }
     }
